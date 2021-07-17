d1eaa68fd6926e761004e4d1640402db128efa7c
Jakowlew <darksmolensksky@gmail.com>
2021-07-18 02:51:36 +0300

    A bit of refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
   
@@ -57,133 +58,31 @@ fromResponse _ = Nothing
 
 main :: IO ()
 main = do
-  auth <- (runLogPassAuth user)
-  print (fromResponse auth)
-  pure ()
+  -- fromResponse: игнорим ошибку серванта, плохо
+  -- fromJust: по сути игнорим ветки ошибки серванта
+  -- т.к. ветки с FailureResponse, InvalidContentTypeHeader
+  -- UnsupportedContentType и DecodeFailure могут содержать данные
+  auth <- fromJust . fromResponse <$> runLogPassAuth user
+  print auth
 
-  -- let token = accessToken auth
-  -- let lp = api (Token token) 
-  -- res <- runMethod $ (lp ^. #getLongPollServer) 
-  --     !  param #needPts   0
-  --     !  param #lpVersion 3
-  --     !? param #groupId   Nothing
+  case auth of
+    LogPassAuthResponse{..} -> do
 
-  -- case res of
-  --   Left err      -> putStrLn $ "Error: " <> show err
-  --   Right serv -> do
-  --     print serv  
-  --     let resp = serv^. #response
-  --     r <- runLp resp $ longPoll 10 234 "a_check" (resp ^. #key) 10 (resp ^. #ts)
-  --     print r
-
-
--- https://server?act=a_check&key=key&ts=ts&wait=wait&mode=mode&version=version
-
-
-{-
-
-interface LongPollResult {
-  // Приходит, если нет ошибок или при failed: 1
-  ts?: number
-  // Приходит, если нет ошибок и при наличии флага 32
-  pts?: number
-  // Приходит, если нет ошибок
-Left (
-  FailureResponse (
-    Request {
-      requestPath = (
-        BaseUrl {
-          baseUrlScheme = Https,
-          baseUrlHost = "oauth.vk.com",
-          baseUrlPort = 443,
-          baseUrlPath = ""
-        },
-        "/token"
-      ),
-      requestQueryString = fromList [
-        ("username",Just "8915634327"),
-        ("password",Just "Vha8124s"),
-        ("grant_type",Just "password"),
-        ("scope",Just "all"),
-        ("client_id",Just "2274003"),
-        ("client_secret",Just "hHbZxrka2uZ6jB1inYsH"),
-        ("2fa_supported",Just "1")
-      ],
-      requestBody = Nothing,
-      requestAccept = fromList [application/json;charset=utf-8,application/json],
-      requestHeaders = fromList []
-  ),
-  requestHttpVersion = HTTP/1.1, requestMethod = "GET"
-  } 
-  (Response 
-    {responseStatusCode = Status 
-  {statusCode = 401, statusMessage = "Unauthorized"}, 
-  responseHeaders = fromList [("Server","kittenx"),("Date","Sat, 17 Jul 2021 22:32:47 GMT"),("Content-Type","application/json; charset=utf-8"),("Content-Length","117"),("Connection","keep-alive"),("X-Powered-By","KPHP/7.4.107880"),("Set-Cookie","remixir=DELETED; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=.vk.com; secure; HttpOnly"),("Set-Cookie","remixlang=7; expires=Sun, 17 Jul 2022 17:55:59 GMT; path=/; domain=.vk.com"),("Cache-control","no-store"),("Content-Encoding","gzip"),("Strict-Transport-Security","max-age=15768000")], responseHttpVersion = HTTP/1.1, responseBody = "{\"error\":\"invalid_client\",\"error_description\":\"Nome utente o password non corretti\",\"error_type\":\"username_or_password_is_incorrect\"}"}))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Left (
-  FailureResponse (
-    Request {
-      requestPath = (
-        BaseUrl {
-          baseUrlScheme = Https,
-          baseUrlHost = "oauth.vk.com",
-          baseUrlPort = 443,
-          baseUrlPath = ""
-        },
-        "/token"
-      ),
-      requestQueryString = fromList [("username",Just "8915634327"),("password",Just "Vha8124s"),("grant_type",Just "password"),("scope",Just "all"),("client_id",Just "2274003"),("client_secret",Just "hHbZxrka2uZ6jB1inYsH"),("2fa_supported",Just "1")],
-      requestBody = Nothing,
-      requestAccept = fromList [application/json;charset=utf-8,application/json],
-      requestHeaders = fromList []
-      ), requestHttpVersion = HTTP/1.1, requestMethod = "GET"}
-      (
-        Response {
-          responseStatusCode = Status {
-            statusCode = 401,
-            statusMessage = "Unauthorized"
-          },
-          responseHeaders = fromList [
-            ("Server","kittenx"),
-            ("Date","Sat, 17 Jul 2021 22:32:47 GMT"),
-            ("Content-Type","application/json; charset=utf-8"),
-            ("Content-Length","117"),
-            ("Connection","keep-alive"),
-            ("X-Powered-By","KPHP/7.4.107880"),
-            ("Set-Cookie","remixir=DELETED; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=.vk.com; secure; HttpOnly"),
-            ("Set-Cookie","remixlang=7; expires=Sun, 17 Jul 2022 17:55:59 GMT; path=/; domain=.vk.com"),
-            ("Cache-control","no-store"),
-            ("Content-Encoding","gzip"),
-            ("Strict-Transport-Security","max-age=15768000")
-          ],
-          responseHttpVersion = HTTP/1.1,
-          responseBody = "{\"error\":\"invalid_client\",\"error_description\":\"Nome utente o password non corretti\",\"error_type\":\"username_or_password_is_incorrect\"}"}))
-  updates?: any[]
-
-  failed?: 1 | 2 | 3 | 4
-
-  // Приходят только при failed: 4
-  min_version?: 0
-  max_version?: 13
-}
-
-
-\"error\":\"invalid_client\",
-\"error_description\":\"Nome utente o password non corretti\",
-\"error_type\":\"username_or_password_is_incorrect\"
-
--}
+      let vk = api $ Token accessToken
+      serv <- fromJust . fromResponse <$> (runMethod $  (vk ^. #getLongPollServer)
+                                                     !  param  #needPts   0
+                                                     !  param  #lpVersion 3
+                                                     !? param  #groupId   Nothing)
+      print serv  
+      -- А вот тут мы проигнорили возможную ошибку от вк
+      let s = serv^. #response
+      event <- fromJust . fromResponse <$> (runLp s $ longPoll
+                                                    ! param #version 10
+                                                    ! param #mode    234
+                                                    ! param #act     "a_check"
+                                                    ! param #key     (s ^. #key)
+                                                    ! param #wait    10
+                                                    ! param #ts      (s ^. #ts))
+      print event
+    -- Явный тип ошибки не дает ее игнорить
+    LogPassAuthError{..} -> putStrLn "Auth error"
