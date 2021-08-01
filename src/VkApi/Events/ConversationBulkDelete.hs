8929dfb2e968f31e80e4677152b3f76f57f5ca40
Borzenkov Andrei <andreyborzenkov2002@gmail.com>
2021-08-01 11:25:12 +0200

    Fix deserialization error

--- src/VkApi/Events/ConversationBulkDelete.hs
+++ src/VkApi/Events/ConversationBulkDelete.hs
@@ -1,12 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
 module VkApi.Events.ConversationBulkDelete where
 
-  import Data.Aeson ( FromJSON, ToJSON )
+import Data.Aeson ( FromJSON(parseJSON), ToJSON )
 
-  import VkApi.Internal.Json ( CamelToSnake(CamelToSnake) )
-  import VkPure.Prelude
+import VkApi.Internal.Json ( CamelToSnake(CamelToSnake) )
+import VkPure.Prelude
+import VkApi.Events.Parsing (parseWithIndex, withArrayByLength)
   
-  data Event = Event
-    { peerId    :: Word32
-    , lastMsgId :: Word32
-    } deriving (Show, Generic)
-    deriving (FromJSON, ToJSON) via CamelToSnake Event
+data Event = Event
+  { peerId    :: Word32
+  , lastMsgId :: Word32
+  } deriving (Show, Generic)
+  deriving ToJSON via CamelToSnake Event
+
+instance FromJSON Event where
+  parseJSON = withArrayByLength "FriendOnlineEvent" (>2) $ \arr -> do
+    peerId    <-  arr `parseWithIndex` 1
+    lastMsgId <-  arr `parseWithIndex` 2
+    pure Event{..}
