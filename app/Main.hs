9e95c05176d3c4bf794119734d41fc5eafdddbef
Borzenkov Andrei <andreyborzenkov2002@gmail.com>
2021-08-04 11:31:39 +0200

    Refactor names

--- app/Main.hs
+++ app/Main.hs
@@ -16,11 +16,13 @@ import VkApi.Messages qualified as Messages
 import VkApi.LongPoll qualified 
 import VkApi.Types ( VkApiResponse(VkSuccessResponse), VkApiSuccess(VkSuccess) )
 
-import VkBot.Utils ( unwrap, ErrorType )
+import VkBot.Utils 
 import VkBot.Auth     qualified as Auth
 import VkBot.LongPoll qualified as LongPoll
 
 import VkPure.Prelude
+
+
 
 user :: Auth.UserCredentials
 user = Auth.UserCredentials "+79067440656" "SteammerHo"
@@ -35,7 +37,7 @@ main = either' . runExceptT $ do
   auth user >>= longPollServer >>= longPollLoop (liftIO . pPrint)
 
 -- // TODO: Use pts param => update types
-longPollLoop :: (VkApi.LongPoll.Success -> ExceptT ErrorType IO ()) -> Messages.LongPollServer -> ExceptT ErrorType IO ()
+longPollLoop :: (VkApi.LongPoll.Success -> ApiError ()) -> Messages.LongPollServer -> ApiError ()
 longPollLoop action server = do
   lpResponse <- longPollUpdates server
 
@@ -44,18 +46,18 @@ longPollLoop action server = do
   let server' = server & #ts .~ (lpResponse ^. #ts)
   longPollLoop action server'
 
-longPollUpdates :: Messages.LongPollServer -> ExceptT ErrorType IO VkApi.LongPoll.Success
+longPollUpdates :: Messages.LongPollServer -> ApiError VkApi.LongPoll.Success
 longPollUpdates s = LongPoll.updates s >>= \case
     VkApi.LongPoll.ResponseSuccess e -> pure e
     _ -> throwE "Can't get updates"
 
-longPollServer :: VkApi.Methods (AsClientT ClientM) -> ExceptT ErrorType IO Messages.LongPollServer
+longPollServer :: VkApi.Methods ResponseM -> ApiError Messages.LongPollServer
 longPollServer vk =
   LongPoll.server vk >>= \case
     VkSuccessResponse s -> pure . coerce $ s
     _ -> throwE "Can't get long poll server"
 
-auth :: Auth.UserCredentials -> ExceptT ErrorType IO (VkApi.Methods (AsClientT ClientM))
+auth :: Auth.UserCredentials -> ApiError (VkApi.Methods ResponseM)
 auth user =
   unwrap (Auth.run user) >>= \case
     VkApi.Auth.ResponseSuccess VkApi.Auth.Success{..} -> pure $ VkApi.methods $ VkApi.Token accessToken
