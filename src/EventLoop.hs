9bea4cddde8a2e0f7c31b4585f8797857c62dfcc
Jakowlew <darksmolensksky@gmail.com>
2021-08-16 01:54:44 +0300

    Use typeclass instead of handler map

--- src/EventLoop.hs
+++ src/EventLoop.hs
@@ -2,67 +2,59 @@ module EventLoop where
 
 import Data.Map.Strict ( (!?), delete, empty, insertWith, Map )
 import Control.Monad.IO.Class
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkIO)
 import UnliftIO.STM
 import UnliftIO.Async
 import System.IO
 import Control.Exception (SomeException, catch)
-import Control.Monad (join)
-type Handler a = a -> IO ()
+import Control.Monad (join, forever)
 
-type EventMap a = Map a [Handler a]
-
-addHandler :: Ord a => a -> [Handler a] -> EventMap a -> EventMap a
-addHandler = insertWith (++)
+class Event a where
+  dispatch :: a -> IO ()
 
 type EventQueue a = TQueue a
 
-pushEvent :: EventQueue a -> a -> STM ()
+newEventQueue :: IO (EventQueue String) 
+newEventQueue = atomically newTQueue
+
+pushEvent :: Event a => EventQueue a -> a -> STM ()
 pushEvent = writeTQueue
 
-readEvent :: EventQueue a -> STM a 
+readEvent :: Event a => EventQueue a -> STM a 
 readEvent = readTQueue
 
--- //TODO: Use generic m instead of IO
-processEvent :: Ord a => EventQueue a -> EventMap a -> IO (EventMap a)
-processEvent q m = join . atomically $ do
-  e <- readEvent q
-  let hs = m !? e
-  case hs of
-    Just hs' -> pure do
-      pooledMapConcurrentlyN_ 100 (\h -> h e) hs'
-      let m' = delete e m
-      pure m'
-    Nothing -> pure (pure m)
+processEvent :: (Event a, Ord a) => EventQueue a -> IO ()
+processEvent q = do
+  e <- atomically $ readEvent q
+  forkIO $ dispatch e
+  pure ()
 
-action :: Show a => a -> IO ()
+-- Testing code
+
+instance Event String where
+  dispatch "0" = action "0"
+  dispatch xs = actionE xs
+
+action :: String -> IO ()
 action e = do
-  print e
+  putStrLn $ "Start " ++ e
   threadDelay $ 3 * 10^6
-  print $ show e ++ " finished"
+  print $ e ++ " finished"
 
-actionE :: Show a => a -> IO ()
+actionE :: String -> IO ()
 actionE e = do
-  print $ show e ++ " ERROR ACTION STARTED"
+  print $ e ++ " ERROR ACTION STARTED"
   threadDelay $ 3 * 10^6
   error "Ooops!"
-  print $ show e ++ " ERROR ACTION ENDED"
+  print $ e ++ " ERROR ACTION ENDED"
   
-newEventQueue :: MonadIO m => m (EventQueue Int) 
-newEventQueue = atomically $ do
-  q <- newTQueue
-  pushEvent q 1
-  pure q
-
-newHandlerMap :: EventMap Int
-newHandlerMap = addHandler 1 acts empty
-  where
-    acts = [action, actionE, action, action]
-
 doStuff :: IO ()
 doStuff = do
   hSetBuffering stdout LineBuffering
   q <- newEventQueue
-  let m = newHandlerMap
-  let process = processEvent q m >> print "Done!"
-  catch process (\(e::SomeException) -> print $ "Handled " ++ show e)
+  forkIO $ forever $ do
+    let handler = \(e::SomeException) -> print "Oaoaoao, exception handled"
+    catch (processEvent q) handler
+  forever $ do
+    x <- getLine
+    atomically $ pushEvent q x
