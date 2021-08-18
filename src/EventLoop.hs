a5f698c0924348340270050906d312d6f1e46f96
Яковлев Георгий <yakovlev.g_qschool@qsoft.ru>
2021-08-18 23:03:09 +0300

    Implement event loop

--- src/EventLoop.hs
+++ src/EventLoop.hs
@@ -1,60 +1,102 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module EventLoop where
 
-import Data.Map.Strict ( (!?), delete, empty, insertWith, Map )
 import Control.Monad.IO.Class
-import Control.Concurrent (threadDelay, forkIO)
+import Control.Monad
+import Control.Concurrent.MVar
+import Control.Concurrent (threadDelay, forkIO, ThreadId)
+import Control.Exception hiding (handle)
 import UnliftIO.STM
 import UnliftIO.Async
 import System.IO
-import Control.Exception (SomeException, catch)
 import Control.Monad (join, forever)
 
-class Event a where
+class EventDispatcher a where
   dispatch :: a -> IO ()
+  dispatchWithState :: EventLoop a -> a -> IO ()
+  dispatchWithState s a = dispatch a
 
 type EventQueue a = TQueue a
 
-newEventQueue :: IO (EventQueue String) 
-newEventQueue = atomically newTQueue
+type EventLoopFlag = MVar Bool
 
-pushEvent :: Event a => EventQueue a -> a -> STM ()
+data EventLoop a = EventLoop
+  { queue :: EventQueue a
+  , flag  :: EventLoopFlag
+  }
+
+newEventQueue :: EventDispatcher a => IO (EventQueue a)
+newEventQueue = newTQueueIO
+
+pushEvent :: EventDispatcher a => EventQueue a -> a -> STM ()
 pushEvent = writeTQueue
 
-readEvent :: Event a => EventQueue a -> STM a 
+readEvent :: EventDispatcher a => EventQueue a -> STM a
 readEvent = readTQueue
 
-processEvent :: (Event a, Ord a) => EventQueue a -> IO ()
-processEvent q = do
-  e <- atomically $ readEvent q
-  forkIO $ dispatch e
-  pure ()
+processEvent :: EventDispatcher a => EventLoop a -> IO ()
+processEvent state@EventLoop{..} = do
+  e <- atomically $ readEvent queue
+  void $ forkIO $ dispatchWithState state e
+
+runEventLoop :: EventDispatcher a => IO (EventLoop a)
+runEventLoop = do
+  state <- newEventLoop
+  forkIO $ eventLoop state
+  pure state
+
+newEventLoop :: EventDispatcher a => IO (EventLoop a)
+newEventLoop = do
+  queue <- newEventQueue
+  flag <- newMVar True
+  pure EventLoop{..}
+
+eventLoop :: EventDispatcher a => EventLoop a -> IO ()
+eventLoop state@EventLoop{..} = do
+  shouldRun <- readMVar flag
+  when shouldRun $ do
+    processEvent state
+    eventLoop state
+
+stopEventLoop :: MVar Bool -> IO ()
+stopEventLoop flag = do
+  _ <- takeMVar flag
+  putMVar flag False
 
 -- Testing code
 
-instance Event String where
-  dispatch "0" = action "0"
-  dispatch xs = actionE xs
-
-action :: String -> IO ()
-action e = do
-  putStrLn $ "Start " ++ e
-  threadDelay $ 3 * 10^6
-  print $ e ++ " finished"
-
-actionE :: String -> IO ()
-actionE e = do
-  print $ e ++ " ERROR ACTION STARTED"
-  threadDelay $ 3 * 10^6
-  error "Ooops!"
-  print $ e ++ " ERROR ACTION ENDED"
-  
 doStuff :: IO ()
 doStuff = do
   hSetBuffering stdout LineBuffering
-  q <- newEventQueue
-  forkIO $ forever $ do
-    let handler = \(e::SomeException) -> print "Oaoaoao, exception handled"
-    catch (processEvent q) handler
+  EventLoop{..} <- runEventLoop
   forever $ do
     x <- getLine
-    atomically $ pushEvent q x
+    atomically $ pushEvent queue x
+
+instance EventDispatcher String where
+  dispatch "ok"  = action  "ok"
+  dispatch _ = actionE "unknown"
+
+  dispatchWithState EventLoop{..} "stop" = actionStop flag "stop"
+  dispatchWithState _ x = dispatch x
+
+action :: String -> IO ()
+action e = do
+  putStrLn $ "Start action: " ++ e
+  threadDelay $ 3 * 10^6
+  print $ "Finish action: " ++ e
+
+actionE :: String -> IO ()
+actionE e = do
+  putStrLn $ "Start action: " ++ e
+  threadDelay $ 3 * 10^6
+  error "Ooops!"
+  print $ "Finish action: " ++ e
+
+actionStop :: MVar Bool -> String -> IO ()
+actionStop f e = do
+  putStrLn $ "Start action: " ++ e
+  threadDelay $ 3 * 10^6
+  stopEventLoop f
+  print $ "Finish action: " ++ e
